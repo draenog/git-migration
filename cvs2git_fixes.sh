906a01534912395a2a701fde9de6ed016fe5a7c1
draenog <draenog>
2011-02-09 09:17:47 +0000

    - clean whitespaces

--- cvs2git_fixes.sh
+++ cvs2git_fixes.sh
@@ -4,12 +4,12 @@
 #
 # It fixes following problems
 #
-# Branches which are no longer labelled can exist in CVS. cvs2git names them 
+# Branches which are no longer labelled can exist in CVS. cvs2git names them
 # unlabeled-<revision>. The script changes "unlabeled" to specified name or
 # deleted them if name is unspecified
 
 # Often in CVS repo only changed file are tagged. In this case cvs2git produces
-# superfluous commits to delete not tagged file. Here the tag in converted git 
+# superfluous commits to delete not tagged file. Here the tag in converted git
 # repo is moved to the parent if the tagged commit:
 # 1. was made by cvs2svn
 # 2. the only change in the tree is deletion of files
@@ -30,7 +30,7 @@ git show-ref --heads | egrep 'unlabeled-[0-9.]+$' | \
 while read rev branchname; do
         if [ -z "$name" ]; then
                 git update-ref -d "$branchname" $rev
-        else        
+        else
                 new_branchname=`echo $branchname | sed -e "s/unlabeled-\([0-9.]\+\)$/${name}-\1/"`
                 if [ "$branchname" != "$new_branchname" ]; then
                         git update-ref "$new_branchname" $rev "" && git update-ref -d "$branchname" $rev
