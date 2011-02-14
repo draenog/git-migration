f9719180237763f21c0555dcf8237e4b145374f2
glen <glen>
2011-02-14 06:46:05 +0000

    - fix filtering

--- tree_filter.sh
+++ tree_filter.sh
@@ -1,8 +1,10 @@
-first_line='# $Revision$, $Date$'
+first_line='# \$Revision\$, \$Date\$'
 log_line='%define date	%(echo `LC_ALL="C" date +"%a %b %d %Y"`)'
 
-find -name \*.spec |
+find -name '*.spec' |
 while read spec; do
-    sed -ie "/^${first_line}$/d
-             /^${log_line}$/,\$d" $spec
+    sed -i -e "
+		/^${first_line}$/d
+		/^${log_line}$/,\$d
+	" $spec
 done
